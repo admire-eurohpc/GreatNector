2636e6f180300ca7d46b3df5a3430ab4d4353971
Pernice Simone <spernice@rstudio1.novalocal>
2023-05-09 12:28:20 +0000

    SP: update functions

--- Main.R
+++ Main.R
@@ -19,9 +19,10 @@ system("mv ./queueHPCmodel.* ./Net")
 # 1. Let cluster the trace in order to identify similar windows in the trace
 # such that we can associate with the same parameter
 
-files = list.files(path = "~/Desktop/GIT/Modelli_GreatMod/HPCmodel/SystemPointView/Data/QuantumEspresso/",pattern = "*.csv")
+files = list.files(path = "./Data/QuantumEspresso/",pattern = "*.csv")
 for(f in files)
-  reference.generation(f,"~/Desktop/GIT/Modelli_GreatMod/HPCmodel/SystemPointView/Data/QuantumEspresso/")
+  reference.generation(x = f,
+                       path = "./Data/QuantumEspresso/")
 
 # 2. Let save places and transitions name.
 name.generation(path = "Net/queueHPCmodel.PlaceTransition",out = "Input/")
@@ -29,7 +30,7 @@ name.generation(path = "Net/queueHPCmodel.PlaceTransition",out = "Input/")
 # 3. Let save the csv file that associates the rate parameter with the right transition 
 # and saves the names of the unknown parameters
 paramenterCSV.generation(csvname = "parametersList.csv",
-                         pathReference = "Input/Reference/dfProva.RDs",
+                         pathReference = "Input/Reference/plot8Deltas.RDs",
                          transitions = "Input/transitionsNAMES.RDS")
 
 #########################
@@ -37,47 +38,50 @@ paramenterCSV.generation(csvname = "parametersList.csv",
 ### Uknown parameter definition ###
 
 paramsName = readRDS("Input/paramsNAMES.RDs")
-params = rep(0,length(paramsName) )
+params = rep(0.001,length(paramsName) )
 names(params) = paramsName
 
-params[c("IO_queue_app1_n1_interval1","IO_queue_app1_n1_interval2")] = c(0,0) 
-params[c("IO_end_app1_n1_interval1","IO_end_app1_n1_interval2")] = c(0,0)
-params[c("IO_start_app1_n1_interval1","IO_start_app1_n1_interval2")] = c(0,0)
-
-params[c("State_start_app1_n1_other3_interval1","State_start_app1_n1_other3_interval2")] = c(.6,0)
-params[c("State_start_app1_n1_mpi2_interval1","State_start_app1_n1_mpi2_interval2")] = c(0,0.6)
-params[c("State_end_app1_n1_other3_interval1","State_end_app1_n1_other3_interval2")] = c(0.5,0)
-params[c("State_end_app1_n1_mpi2_interval1","State_end_app1_n1_mpi2_interval2")] = c(0,0.5)
+# params[c("IO_queue_app1_n1_interval1","IO_queue_app1_n1_interval2")] = c(0,0) 
+# params[c("IO_end_app1_n1_interval1","IO_end_app1_n1_interval2")] = c(0,0)
+# params[c("IO_start_app1_n1_interval1","IO_start_app1_n1_interval2")] = c(0,0)
+# 
+# params[c("State_start_app1_n1_other3_interval1","State_start_app1_n1_other3_interval2")] = c(.6,0)
+# params[c("State_start_app1_n1_mpi2_interval1","State_start_app1_n1_mpi2_interval2")] = c(0,0.6)
+# params[c("State_end_app1_n1_other3_interval1","State_end_app1_n1_other3_interval2")] = c(0.5,0)
+# params[c("State_end_app1_n1_mpi2_interval1","State_end_app1_n1_mpi2_interval2")] = c(0,0.5)
 
 ###################################
+
+# every 100 steps we have one second!
 
 model.analysis(solver_fname = "./Net/queueHPCmodel.solver",
                parameters_fname = "Input/parametersList.csv",
                functions_fname = "./RFunction/QueueFunctions.R",
-               f_time = 50,
+               f_time = 500,
                s_time = 1,
                i_time = 0,
-               n_run = 1,
-               solver_type = "LSODA",
+               n_run = 5,
+               solver_type = "SSA",
                parallel_processors = 1,
-               ini_v = params,
-               debug = T )
+               ini_v = params)
 
 
 ModelAnalysisPlot("queueHPCmodel_analysis/queueHPCmodel-analysis-1.trace")
 
 ### Calibration 
+ref <- read.csv("Data/QuantumEspresso/plot8Deltas.csv") %>% na.omit()
+maxTime = max(ref[,"X"])
 
-model.calibration(solver_fname = "./Net/HPCmodel.solver",
-                  parameters_fname = "Input/parametersList.csv",
-                  functions_fname = "./RFunction/Functions.R",
-                  reference_data = "./Input/ReferenceCl2.csv",
+model.calibration(solver_fname = "./Net/queueHPCmodel.solver",
+                  parameters_fname = "./Input/parametersList.csv",
+                  functions_fname = "./RFunction/QueueFunctions.R",
+                  reference_data = "./Input/Reference/plot8Deltas.csv",
                   distance_measure = "error",
                   parallel_processors = 10,
-                  f_time = 5*60, # simulo 5 minuti
+                  f_time = (maxTime)*100,
                   s_time = 1,
                   i_time = 0,
-                  n_run = 500,
+                  n_run = 250,
                   lb_v = rep(0,length(paramsName)),
                   ub_v = rep(1,length(paramsName)),
                   ini_v = rep(0.5,length(paramsName)),
